73ee7c8c4ff54258cf15351805ff3edc31b7b7db
hidao80 <hidao80@users.noreply.github.com>
2022-10-16 16:03:32 +0900

    improve: Added a keyboard shortcut to open the extension options screen.

--- js/background.js
+++ js/background.js
@@ -2,9 +2,14 @@ import { outputLog } from "./lib/download.js";
 import { Log } from "./lib/logger.js";
 
 chrome.commands.onCommand.addListener((command) => {
-    Log.log(`background!${command}`);
     if (command == "log_download") {
         outputLog();
+    } else if (command == "open_options") {
+        if (chrome.runtime.openOptionsPage) {
+            chrome.runtime.openOptionsPage();
+        } else {
+            window.open(chrome.runtime.getURL('html/options.html'));
+        }
     }
 }
 );
