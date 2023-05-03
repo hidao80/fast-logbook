57697f2465ec61b6fe4e7bfe398c172654bfd29f
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-05-03 19:29:00 +0900

    fix: Log screens can now be entered.

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 import { $, syncGet, syncSet, LOG_DATA_KEY, trimNewLine, appendTime } from "./lib/utils.js";
-import { i18nInit, translate } from "./lib/i18n.js";
+import { i18nInit } from "./lib/i18n.js";
 import { downloadLog } from "./lib/download.js";
 import { Log } from "./lib/logger.js";
 
@@ -36,11 +36,11 @@ async function loadLogs() {
 /**
  * Save your work log
  *
- * @param {string} log
  * @return {Promise}
  */
-async function saveLogs(log) {
-    await syncSet({ [LOG_DATA_KEY]: trimNewLine(log) });
+async function saveLogs() {
+    const textarea = $('textarea');
+    await syncSet(LOG_DATA_KEY, trimNewLine(textarea.value));
 }
 
 /**
@@ -49,59 +49,72 @@ async function saveLogs(log) {
 document.addEventListener("DOMContentLoaded", async () => {
     i18nInit();
 
-    for (const node of $('label')) {
-        translate(node);
+    // When you click on a preset tag
+    for (const node of $('label[data-shortcut-key]')) {
+        node.textContent = await syncGet('shortcut_' + node.dataset.shortcutKey);
+        if (node.textContent == "undefined") {
+            node.textContent = translate(node.dataset.i18n);
+        }
+
+        node.addEventListener('click', async function(e) {
+            e.stopPropagation();
+            if (document.activeElement.value) return;
+            await appendLog(appendTime(this.textContent))
+            await saveLogs();
+        });
     }
 
-    // When the entry to the 0th is confirmed, the log entered is imprinted.
-    $('input').addEventListener('change', async function() {
-        await appendLog(appendTime(this.value));
+    // When a key is entered on the popup
+    document.body.addEventListener('keydown', async (e) => {
+        if (document.activeElement.value) return;
+
+        // When a numeric key is pressed
+        const matches = e.code.match(/Digit(\d)/);
+        if (matches?.length == 2) {
+            const inputDigit = matches[1];
+            if (inputDigit == '0') {
+                // When 0 is pressed, focus on the input field.
+                e.preventDefault()
+                e.stopPropagation();
+                $('input').focus();
+                $('input').value = '';
+            } else {
+                // For 1-9, imprint the preset tag.
+                const node = $(`label[data-shortcut-key="${inputDigit}"]`);
+                await appendLog(appendTime(node.textContent));
+            }
+        } else if (["Escape", "Alt+Shift+0"].includes(e.code)) {
+            await saveLogs();
+            window.close();
+        }
     });
 
     // When the download button is pressed, the log is downloaded.
     $('button').addEventListener('click', downloadLog);
 
-    // When a key is entered on the popup
-    document.body.addEventListener('keydown', async (e) => {
-        // e.stopPropagation();
-        if (document.activeElement.value) return;
-
-        // When a numeric key is pressed
-        if (/Digit\d/.test(e.code)) {
-            if (e.code == 'Digit0') {
-                // e.preventDefault();
-                $('input').focus();
-            } else {
-                // For 1-9, imprint the preset tag.
-                const digit = e.code.slice(-1);
-                const node = $(`[data-shortcut-key="${digit}"]`);
-                await appendLog(appendTime(node.value || node.textContent));
-            }
-        } else if (e.code == "Enter" || e.code == "Escape" || e.code == "Alt+Shift+0") {
-            await saveLogs($('textarea').value);
-            window.close();
+    // When the entry to the 0th is confirmed, the log entered is imprinted.
+    $('input').addEventListener('keydown', async function(e) {
+        if ("Enter" == e.code) {
+            // e.stopPropagation();
+            if (this.value.length == 0) return;
+            await appendLog(appendTime(this.value));
+            this.value = '';
         }
-    });
-
-    // When you click on a preset tag
-    for (const node of $('label[data-shortcut-key]')) {
-        node.addEventListener('click', async function(e) {
-            e.stopPropagation();
-            if (document.activeElement.value) return;
-            await appendLog(appendTime(this.textContent))
-        });
-    }
-
-    // When a popup loses focus, it saves the contents of the textarea.
-    window.addEventListener('blur', async function() {
-        await saveLogs($('textarea').value);
     });
 
     //
     $('textarea').addEventListener('keydown', async function(e) {
-        if (e.code == "Enter" || e.code == "Escape" || e.code == "Alt+Shift+0") {
-            await saveLogs(this.value);
+        if ("Enter" == e.code) {
+            // e.stopPropagation();
+            await saveLogs();
         }
+    });
+
+    // When a popup loses focus, it saves the contents of the textarea.
+    [$('input'), window].forEach((node) => {
+        node.addEventListener('blur', async function() {
+            await saveLogs();
+        });
     });
 
     // Load the last saved log
